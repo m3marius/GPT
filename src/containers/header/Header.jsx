57f0f44cf51825ea146cfe64fb5a0a467fc665f2
m3 <m3mariusdev@gmail.com>
2023-03-15 19:20:10 +0000

    First commit

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
+
+const Header = () => {
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Discover the Power of AI: Revolutionizing Industries with Innovative
+          Solutions from Our Next-Generation Startup Harnessing Cutting-Edge
+          Technology and Expertise to Empower Your Future Join Us in Redefining
+          the Limits of What's Possible.
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="emails" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access in the last 24 hours</p>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
